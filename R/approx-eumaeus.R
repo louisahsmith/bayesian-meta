de1b43c235c12d7f511bb3d4e5fab748faebaf7d
Louisa Smith <louisahsmith@gmail.com>
2022-01-07 11:47:32 -0500

    interpolate before fitting model to deal with irregular grid

--- R/approx-eumaeus.R
+++ R/approx-eumaeus.R
@@ -6,10 +6,9 @@ library(furrr)
 rstan_options(auto_write = TRUE)
 
 # not all have positive controls
-# and seem to be not on regular grids
 # eumaeus_CohortMethod_1_21184.rds has 39 positive controls with multiple sites
 # eumaeus_CohortMethod_1_21215.rds has 294 positive controls with multiple sites
-eumaeus_file <- "eumaeus_CohortMethod_1_21215.rds"
+eumaeus_file <- "eumaeus_HistoricalComparator_1_21215.rds"
 
 all_dat <- read_rds(here::here("data", eumaeus_file)) %>% 
   mutate(site = as.numeric(factor(databaseId))) %>% 
@@ -21,7 +20,10 @@ positive_control_outcomes <- all_dat %>%
   select(outcomeId, negativeControlId, effectSize) %>% 
   distinct()
 
-sim_function <- function(i, positive_control_outcomes, all_dat, iter = 3000, chains = 1, regular = TRUE, ...) {
+sim_function <- function(i, positive_control_outcomes, all_dat, 
+                         min_val = log(.1), max_val = log(10), n_vals = 1000,
+                         iter = 3000, chains = 1, refresh = 0,
+                         pars = c("BETA", "THETA", "theta_i", "beta"), ...) {
   
   # choose a single positive control
   positive_control_i <- positive_control_outcomes[i,]
@@ -35,21 +37,36 @@ sim_function <- function(i, positive_control_outcomes, all_dat, iter = 3000, cha
   
   # these are the estimates (profile likelihoods) from each of the sites
   # that had that positive control
-  ests <- all_dat %>% 
+  init_ests <- all_dat %>% 
     filter(outcomeId == outcome, !is.na(seLogRr), 
            # remove any without concave likelihood within bounds
-           between(rr, 0.1, 10)) %>% 
+           between(logRr, min_val, max_val)) %>% 
     group_by(databaseId) %>% 
     # just use last period
     filter(periodId == max(periodId)) %>% 
-    ungroup() %>% 
-    select(site, ll) %>% 
-    unnest(ll)
+    ungroup() 
   
-  if (nrow(ests) == 0) return(NULL)
-  sites <- unique(ests$site)
-  
+  # skip if no estimates
+  if (nrow(init_ests) == 0) return(NULL)
+  # skip if only 1 site (can't really meta-analyze (stan code doesn't work because expects vector)... will do elsewhere)
+  sites <- unique(init_ests$site)
   if (length(sites) < 2) return(NULL)
+  
+  # if the grid is not regular, interpolate first
+  # note that this really just checks that there are the right number, 
+  # not necessarily that they are evenly spaced, but we will assume 
+  # if the right number, they are regular
+  if (!all(map_lgl(init_ests$ll, ~nrow(.x) == n_vals))) {
+    ests <- init_ests %>% 
+      mutate(new_ll = map(ll, ~as_tibble(approx(.x$point, .x$value, xout = seq(min_val, max_val, length.out = n_vals))))) %>% 
+      select(site, new_ll) %>% 
+      unnest(new_ll) %>% 
+      rename("point" = x, "value" = y)
+  } else {
+    ests <- init_ests %>% 
+      select(site, ll) %>% 
+      unnest(ll)
+  }
   
   # this is just helping to index the log-likelihood approximations
   for_grid <- ests %>% 
@@ -61,7 +78,6 @@ sim_function <- function(i, positive_control_outcomes, all_dat, iter = 3000, cha
   # generate the positive control of interest
   # so we exclude them from the negative controls
   # as well as any sites that don't have that positive control
-  
   NCs_all <- all_dat %>% 
     filter(is.na(effectSize),
            !is.na(seLogRr), 
@@ -83,35 +99,30 @@ sim_function <- function(i, positive_control_outcomes, all_dat, iter = 3000, cha
               ll = ests$value, # log likelihood estimates
               vals_evaled = ests$point, # values at which the log likelihood was estimated
               ll_indices = mutate(group_by(ests, site), rowid = row_number())$rowid, # indexes the log likelihood within site
-              grid_id = matrix(for_grid$rowid, ncol = 2, byrow = TRUE), # start and end indices of the log-likehood vectors
+              grid_id = matrix(for_grid$rowid, ncol = 2, byrow = TRUE), # start and end indices of the log-likelihood vectors
+              # recalculate in case regular but forgot to provide appropriate sequence
               n_vals = tapply(ests$point, ests$site, length), 
               min_vals = tapply(ests$point, ests$site, min),
               max_vals = tapply(ests$point, ests$site, max)) 
   
-  priors <- list(
-    mean_prior_BETA = 0,
-    sd_prior_BETA = 4,
-    mean_prior_TAU = 0,
-    sd_prior_TAU = 1,
-    mean_prior_beta = rep(0, length(sites)),
-    sd_prior_beta = rep(1, length(sites)),
-    mean_prior_tau = rep(0.5, length(sites)),
-    sd_prior_tau = rep(1, length(sites)),
-    mean_prior_THETA = 0,
-    sd_prior_THETA = 4,
-    mean_prior_GAMMA = 0.5,
-    sd_prior_GAMMA = 2)
+  priors <- list(mean_prior_BETA = 0,
+                 sd_prior_BETA = 4,
+                 mean_prior_TAU = 0,
+                 sd_prior_TAU = 1,
+                 mean_prior_beta = rep(0, length(sites)),
+                 sd_prior_beta = rep(1, length(sites)),
+                 mean_prior_tau = rep(0.5, length(sites)),
+                 sd_prior_tau = rep(1, length(sites)),
+                 mean_prior_THETA = 0,
+                 sd_prior_THETA = 4,
+                 mean_prior_GAMMA = 0.5,
+                 sd_prior_GAMMA = 2)
   
-  if (regular) {
   mod <- stan(here::here("stan", "NCs-multiple-sites-priors-real-effect-likelihood.stan"),
               data = c(dat, priors), iter = iter, chains = chains,
+              pars = pars, refresh = refresh,
               control = list(adapt_delta = 0.9999, max_treedepth = 15))
-  
-  } else {
-  mod <- stan(here::here("stan", "NCs-multiple-sites-priors-real-effect-likelihood-irregular.stan"),
-              data = c(dat, priors), iter = iter, chains = chains,
-              control = list(adapt_delta = 0.9999, max_treedepth = 15))
-  }
+
   
   # extract the site-specific thetas (true effect) and betas (mean bias)
   little_thetas <- mod %>% 
@@ -140,10 +151,9 @@ sim_function <- function(i, positive_control_outcomes, all_dat, iter = 3000, cha
   to_return
 }
 
-
 plan(multisession)
 res <- future_map(1:nrow(positive_control_outcomes), sim_function, 
-                    positive_control_outcomes, all_dat, iter = 2000, chains = 3, regular = TRUE)
+                    positive_control_outcomes, all_dat, iter = 2000, chains = 3)
 plan(sequential)
 
 write_rds(res, here::here("results", paste0("res_", eumaeus_file)))
